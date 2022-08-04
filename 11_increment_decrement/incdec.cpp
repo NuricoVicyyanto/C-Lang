376a7d55558f8ee579c7d3f7b3175f4f2afa5efe
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2022-08-04 19:19:38 +0700

    increment decrement

---
+++ 11_increment_decrement/incdec.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // increment and decrement
+    // preincrement and postincrement
+
+    int a = 8;
+    int b = 9;
+
+    // post increment
+    cout << a << endl;
+    cout << a++ << endl;
+    cout << a << endl;
+
+    cout << "========" << endl;
+
+    cout << b << endl;
+    cout << ++b << endl;
+    cout << b << endl;
+
+}
