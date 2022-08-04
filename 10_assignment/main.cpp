572edd31a411c919fc92a1ad3190a08fe0664067
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2022-08-04 19:08:06 +0700

    assignment operator

---
+++ 10_assignment/main.cpp
@@ -0,0 +1,22 @@
+# include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 8;
+    
+
+    // assignment operator dibuat agar singkat +=, -=, *=, /=
+    a += 2;
+    cout << a << endl;
+
+    a -= 1;
+    cout << a << endl;
+
+    a *= 2;
+    cout << a << endl;
+
+    a /= 3;
+    cout << a << endl;
+}
