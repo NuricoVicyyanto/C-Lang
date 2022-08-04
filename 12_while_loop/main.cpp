cc046eaee4b0953ea764c58556dbc93a8a164502
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2022-08-04 20:00:42 +0700

    while

---
+++ 12_while_loop/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 8;
+    while (a <= 10)
+    {
+        cout << "hai ";
+        cout << a << endl;
+        a += 1;
+    }
+
+    cout << "selesai" << endl;
+
+    cin.get();
+    return 0;
+}
