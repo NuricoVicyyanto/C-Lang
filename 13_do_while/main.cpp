f24feb811acae907ca3fef4f9f90ab843b250b0a
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2022-08-04 20:07:45 +0700

    do while

--- 13_do_while/main.cpp
+++ 13_do_while/main.cpp
@@ -4,5 +4,16 @@ using namespace std;
 
 int main()
 {
-    
+    int a = 1;
+
+    do
+    {
+        cout << "hai ";
+        cout << a << endl;
+        a++;
+    } while (a <= 10);
+
+    cout << "selesai" << endl;
+    cin.get();
+    return 0;
 }
