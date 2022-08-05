28846a9dc16f308b402afab36e108c0ae435284d
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2022-08-05 20:11:09 +0700

    fungsi cmath

---
+++ 18_fungsi/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// isi library cmath
+// https://cplusplus.com/reference/cmath/
+
+int main()
+{
+    int n;
+
+    cout << "masukkan nilai : ";
+    cin >> n;
+
+    cout << "hasil akar dari n adalah : " << sqrt(n) << endl;
+
+
+}
