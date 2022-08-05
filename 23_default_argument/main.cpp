86a0d43abb026ea7ef8b287fc6c03027af5a2dcd
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2022-08-05 23:09:27 +0700

    default argument

---
+++ 23_default_argument/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+// default argument => memberi nilai default pada variabel untuk menghindari nilai kosong
+double vol_kubus(double a, double b, double c = 2);
+
+int main()
+{
+    cout << "volume kubus adalah : " << vol_kubus(2, 3, 5) << endl;
+    cout << "volume kubus adalah : " << vol_kubus(2, 3) << endl;
+}
+
+double vol_kubus(double a, double b, double c)
+{
+    return a * b * c;
+}
