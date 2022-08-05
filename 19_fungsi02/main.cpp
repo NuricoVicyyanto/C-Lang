bbea8adbe3ae959a98d87604530bb0800caf40ef
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2022-08-05 21:12:12 +0700

    void function

--- 19_fungsi02/main.cpp
+++ 19_fungsi02/main.cpp
@@ -2,11 +2,20 @@
 
 using namespace std;
 
+// fungsi dengan kembalian : return => mengembalika nilai
+// reporter
 int pangkat(int y)
 {
     int x;
     x = y * y;
     return x;
+}
+
+// fungsi void tanpa return => tidak mengembalikan nilai
+// worker
+void cetak(int text)
+{
+    cout << text << endl;
 }
 
 int main()
@@ -15,7 +24,8 @@ int main()
     cout << "masukkan nilai :";
     cin >> n;
 
-    cout << "hasil pangkat adalah : " << pangkat(n) << endl;
+    cout << "hasil pangkat adalah : ";
+    cetak(pangkat(n)); // dipanggil dari fungsi void dan return
 
     cin.get();
     return 0;
